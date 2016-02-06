3c0acfc503e0a40a25962fdc28f3be007cdd57d0
kojiyamauchi <all.the.small.things@me.com>
2016-02-06 22:50:18 +0900

    first commit again

---
+++ index.php
@@ -0,0 +1,142 @@
+<!doctype html>
+<html lang="ja">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="content-language" content="ja">
+  <title>All The Small Things, | www.kojiyamauchi.com</title>
+  <meta name="title" content="koji yamauchi" />
+  <meta name="viewport" content="width=800">
+  <meta name="description" content="Koji Yamauchi's portfolio site. Live In Tokyo, Japan. I'm Freelance Web Designer, Web Developer and Graphics. Welcome to my canvas. I Make More.">
+  <meta name="keywords" content="graphic,fasion,music,punk,All The Small Things,kojiyamauchi.com,koji yamauchi,kojiyamauchi,kojiyyyyamauchi,ヤマウチコウジ, ヤマウチ コウジ,山内康次,山内 康次,Web Design,Web Designer,Web Development,Web Developer,ウェブデザイン,ウェブデザイナー,ウェブデベロップ,ウェブデベロッパー, グラフィック,絵,音楽,洋服">
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="http://kojiyamauchi.com" />
+  <meta property="og:title" content="All The Small Things," />
+  <meta property="og:description" content="Koji Yamauchi's Portfolio Web Site." />
+  <meta property="og:image" content="http://kojiyamauchi.com/index_files/images/allTheSmallThingsIMG.jpg" />
+  <meta property="og:site_name" content="kojiyamauchi.com" />
+  <link rel="apple-touch-icon" href="images/favicons/allTheSmallThingsLogoWebClip.png" />
+  <link rel="shortcut icon" href="images/favicons/favicon.ico" />
+  <link rel="icon" type="image/png" href="images/favicons/favicon-32x32.png" sizes="32x32">
+  <link rel="icon" type="image/png" href="images/favicons/favicon-96x96.png" sizes="96x96">
+  <link rel="icon" type="image/png" href="images/favicons/favicon-16x16.png" sizes="16x16">
+  <link rel="mask-icon" href="images/favicons/safari-pinned-tab.svg" color="#5bbad5">
+  <meta name="msapplication-TileColor" content="#da532c">
+  <meta name="msapplication-TileImage" content="images/favicons/mstile-144x144.png">
+  <meta name="theme-color" content="#dbdbdb">
+  <link rel="stylesheet" type="text/css" href="css/animate.css" />
+  <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="css/allTheSmallThings.css">
+  <script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+  <script type="text/javascript" src="js/jquery-ui.min.js"></script>
+  <script type="text/javascript" src="js/jquery.lettering.js"></script>
+  <script type="text/javascript" src="js/jquery.textillate.js"></script>
+  <script type="text/javascript" src="js/jquery.transform2d.js"></script>
+  <script type="text/javascript" src="js/raphael-min.js"></script>
+  <script type="text/javascript" src="js/jquery.lazylinepainter-1.7.0.min.js"></script>
+  <script type="text/javascript" src="js/googleAnalytics.js"></script>
+  <script type="text/javascript" src="js/allTheSmallThings.js"></script>
+</head>
+
+<body>
+
+  <!---------------------------------------------------- Default Display. ---------------------------------------------------->
+  <div id="contents1">
+    <header>
+      <h1 id="mainLogo"><a href="http://kojiyamauchi.com/main/" class="introduction">All The Small<br>Things,</a></h1> </header>
+    <div id="siteDescription">
+      <section>
+        <div id="mainDescription">
+          <h2 id="kojiyamauchi">www.kojiyamauchi.com</h2>
+          <h2 id="myPortfolio">koji yamauchi's portfolio site</h2>
+          <div id="mainDescriptionBackIMG"> <img src="images/hoveraction1.gif" width="" height="" alt="" /> </div>
+        </div>
+        <div id="subDescription">
+          <h3 id="myDescription">Web Design & Development. Welcome to my canvas, I Make More.</h3>
+          <div id="subDescriptionBackIMG"> <img src="images/hoveraction2.gif" width="" height="" alt="" /> </div>
+        </div>
+      </section>
+    </div>
+    <div id="socialLink">
+      <section>
+        <h4>Social Networks</h4>
+        <ul>
+          <li><a href="https://twitter.com/KojiyyyYamauchi" target="_blank" class="introduction">Twitter</a></li>
+          <li><a href="http://www.facebook.com/kojiyyyyamauchi" target="_blank" class="introduction">Facebook</a></li>
+          <li><a href="https://www.linkedin.com/in/kojiyyyyamauchi" target="_blank" class="introduction">LinkedIn</a></li>
+          <li><a href="http://www.behance.net/kojiyamauchi" target="_blank" class="introduction">Behance</a></li>
+          <li><a href="https://github.com/kojiyamauchi/" target="_blank" class="introduction">GitHub</a></li>
+          <li><a href="http://instagram.com/kojiyamauchi" target="_blank" class="introduction">Instagram</a></li>
+        </ul>
+      </section>
+    </div>
+    <footer>
+      <p>© 2015 All The Small Things, All Rights Reserved. </p>
+    </footer>
+  </div>
+  <!---------------------------------------------------- Default Display End. ----------------------------------------------------->
+
+  <!----------------------------- Case. Motion Skip Button Click Display. Default Display None. ----------------------------->
+  <div id="contents2">
+    <header>
+      <h1 id="mainLogo"><a href="http://kojiyamauchi.com/main/">All The Small<br>Things,</a></h1> </header>
+    <div id="siteDescription">
+      <section>
+        <div id="mainDescription">
+          <h2 id="kojiyamauchi">www.kojiyamauchi.com</h2>
+          <h2 id="myPortfolio">koji yamauchi's portfolio site</h2>
+          <div id="mainDescriptionBackIMG"> <img src="images/hoveraction1.gif" width="" height="" alt="" /> </div>
+        </div>
+        <div id="subDescription">
+          <h3 id="myDescription">Web Design & Development. Welcome to my canvas, I Make More.</h3>
+          <div id="subDescriptionBackIMG"> <img src="images/hoveraction2.gif" width="" height="" alt="" /> </div>
+        </div>
+      </section>
+    </div>
+    <div id="socialLink">
+      <section>
+        <h4>Social Networks</h4>
+        <ul>
+          <li><a href="https://twitter.com/KojiyyyYamauchi" target="_blank">Twitter</a> </li>
+          <li><a href="http://www.facebook.com/kojiyyyyamauchi" target="_blank">Facebook</a></li>
+          <li><a href="https://www.linkedin.com/in/kojiyyyyamauchi" target="_blank">LinkedIn</a></li>
+          <li><a href="http://www.behance.net/kojiyamauchi" target="_blank">Behance</a></li>
+          <li><a href="https://github.com/kojiyamauchi/" target="_blank">GitHub</a></li>
+          <li><a href="http://instagram.com/kojiyamauchi" target="_blank">Instagram</a></li>
+        </ul>
+      </section>
+    </div>
+    <footer>
+      <p>© 2015 All The Small Things, All Rights Reserved. </p>
+    </footer>
+  </div>
+  <!----------------------------------------- Case. Motion Skip Button Click Display End. ----------------------------------------->
+
+  <div id="logoSVG"></div>
+  <div id="introWelcome"> <span class="allTheSmallThingsLogoWrap">
+      <span class="logoSneaker">
+        <img src="images/allTheSmallThingsLogoSneaker.png">
+      </span> <span class="logoNote hopping">
+        <img src="images/allTheSmallThingsLogoNote.png">
+      </span> <span class="logoMask">
+        <img src="images/allTheSmallThingsLogoMask.png">
+      </span> </span>
+    <!-- <span class="fa fa-paper-plane-o fa-5x hopping"></span>&nbsp;&nbsp;--><span class="welcomeFont hopping">W</span><span class="welcomeFont hopping">e</span><span class="welcomeFont hopping">l</span><span class="welcomeFont hopping">c</span><span class="welcomeFont hopping">o</span><span class="welcomeFont hopping">m</span><span
+    class="welcomeFont hopping">e</span><span class="welcomeFont hopping">!</span><span class="welcomeFont hopping">!</span><span class="welcomeFont hopping">!</span><!--&nbsp;&nbsp;<span class="fa fa-paper-plane-o fa-5x hopping"></span>--><span class="allTheSmallThingsLogoWrap">
+      <span class="logoSneaker">
+        <img src="images/allTheSmallThingsLogoSneaker.png">
+      </span> <span class="logoNote hopping">
+        <img src="images/allTheSmallThingsLogoNote.png">
+      </span> <span class="logoMask">
+
+
+        <img src="images/allTheSmallThingsLogoMask.png">
+      </span> </span>
+  </div>
+  <div id="motionSkip">
+    <p>Motion
+      <br>Skip!!!</p>
+  </div>
+</body>
+
+</html>
