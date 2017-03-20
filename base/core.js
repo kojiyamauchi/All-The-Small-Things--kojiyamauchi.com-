33a3e89e73ae18babd866cd7ae46f7b091fbb55d
kojiyamauchi <all.the.small.things@me.com>
2017-03-20 18:24:17 +0900

    add base/ directory

---
+++ base/core.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var THREE = require('three');
+
+
+
+var jQuery = require('jQuery');
+
+jQuery(function ($) {
+    $('body').css({
+        'background-color': '#fff'
+    });
+});
